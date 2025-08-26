c1412c84cdf0f9d04be0e3b6040401f0d2c5178c
Matheus Dias <matheusmelhor3@outlook.com>
2025-08-26 19:45:21 -0300

    inital commit

---
+++ src/test/java/utfpr/OD46S/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.OD46S.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
